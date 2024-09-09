7cbd723a713d949518ef2b17b6062a88e4d825e8
FLIPPHXNE <134173491+FLIPPHXNE@users.noreply.github.com>
2024-09-09 10:51:38 +0500

    Test

--- src/main/java/org/Control1/Task3.java
+++ src/main/java/org/Control1/Task3.java
@@ -6,11 +6,11 @@ public class Task3 {
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите N:");
         int N = sc.nextInt();
-        double sum = 1;
+        double sum = 0;
         int factorial = 1;
 
-        for(int i = 1; i <= N; i++){
-            factorial *= i;
+        for(int i = 0; i < N; i++){
+            if(i!= 0) factorial *= i;
             sum += 1 / (double)factorial;
         }
         System.out.printf("Частичная сумма равна %.5f", sum);
